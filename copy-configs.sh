c60454f33f7bdac86b05d52fb936524f5d5ee7bd
Idlidev <idliyout@gmail.com>
2021-11-19 18:45:26 +0530

    Updated config install script

---
+++ copy-configs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+cd ~/.dotfiles
+for dir in $( find .config -maxdepth 2 -mindepth 2 | awk -F "/" '{print $NF}')
+do
+    rm -rf ~/$dir
+done
+
+for dir in $(find .home -mindepth 2 -maxdepth 2 | awk -F "/" '{print $NF}')
+do
+    rm -rf ~/$dir
+done
+
+dots=$(ls -d ~/.dotfiles/.config/*)
+for dot in $dots
+do
+    cp -r "$dot" ~/.config
+done
+
+mkdir -p ~/Pictures
+mkdir -p ~/Pictures/wallpapers
+
+wp-folder=$(ls -d ~/.dotfiles/.assets/wallpapers/*)
+for wallpaper in $wp-folder
+do
+    cp -r "wallpaper" ~/Pictures/wallpapers
+done
+
+cp -r ~/.dotfiles/.home/.local/share/rofi ~/.local/share
+cp -r ~/.dotfiles/.home/.zshrc ~
