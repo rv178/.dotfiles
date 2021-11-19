e2927df4ed458a3542de39db558222420f193a88
Idlidev <idliyout@gmail.com>
2021-11-19 13:42:50 +0530

    Updated install script

--- install-configs.sh
+++ install-configs.sh
@@ -1,23 +1,27 @@
-rm -rf ~/.config/rofi ~/.local/share/rofi ~/.config/ranger ~/.config/dunst ~/.zprofile ~/.xinitrc ~/.config/sxhkd ~/.config/betterlockscreenrc ~/.config/bspwm ~/.config/nvim ~/.config/polybar ~/.config/zathura ~/.zshrc
-
-cd ~/.dotfiles/.home/
-homefiles=$(ls -d */)
-for home in $homefiles
-do 
-	stow -D $home -t ~/
-	stow $home -t ~/
+#!/usr/bin/env bash
+cd ~/.dotfiles
+for dir in $( find .config -maxdepth 2 -mindepth 2 | awk -F "/" '{print $NF}')
+do
+    rm -rf ~/$dir
 done
 
-cd ~/.dotfiles/.config/
+for dir in $(find .home -mindepth 2 -maxdepth 2 | awk -F "/" '{print $NF}')
+do
+    rm -rf ~/$dir
+done
+
+cd ~/.dotfiles/.config
 dots=$(ls -d */)
 for dot in $dots
 do
-	stow -D $dot -t ~/.config
-	stow $dot -t ~/.config
+    stow -D "$dot" -t ~/.config
+    stow "$dot" -t ~/.config
 done
 
-mkdir -p ~/Pictures/wallpapers
-sudo cp ~/.dotfiles/.assets/wallpapers/* ~/Pictures/wallpapers
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cd ~/.dotfiles/.home
+dots=$(ls -d */)
+for dot in $dots
+do
+    stow -D "$dot" -t ~/
+    stow "$dot" -t ~/
+done
