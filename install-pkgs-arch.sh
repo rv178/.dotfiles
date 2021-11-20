2038a1d1d1383f22c2ac3dc5d26fec8bcd7d20e6
Idlidev <idliyout@gmail.com>
2021-11-20 20:47:14 +0530

    Updated install script

--- install-pkgs-arch.sh
+++ install-pkgs-arch.sh
@@ -11,13 +11,13 @@ sudo make clean install
 
 # Add chaotic AUR support (optional)
 
-# pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-# pacman-key --lsign-key 3056513887B78AEB
+# sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+# sudo pacman-key --lsign-key 3056513887B78AEB
 
-# pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+# sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-# echo '[chaotic-aur]' >> /etc/pacman.conf
-# echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+# sudo echo '[chaotic-aur]' >> /etc/pacman.conf
+# sudo echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
 
 # Synchronize package databases 
 sudo aura -Syu
