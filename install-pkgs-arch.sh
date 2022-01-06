63a03fddc7c91fa61805cc13b61add9c59a6632d
Idlidev <idliyout@gmail.com>
2022-01-06 13:39:35 +0530

    updated install script

--- install-pkgs-arch.sh
+++ install-pkgs-arch.sh
@@ -1,15 +1,26 @@
+Red='\e[0;31m';
+Gre='\e[0;32m';
+Yel='\e[0;33m';
+Cya='\e[0;36m';
+Whi='\e[0;37m';
+
 # Synchronize package databases 
+
+echo -e "[${Red}*${Whi}] Updating system.."
 sudo pacman -Syu
 
 # Install git
+echo -e "[${Red}+${Whi}] Installing git"
 sudo pacman -S --noconfirm --needed git 
 
 # Install aura AUR helper
+echo -e "[${Red}+${Whi}] Installing aura"
 git clone https://aur.archlinux.org/aura-bin.git /tmp/aura-git-cloned
 cd /tmp/aura-git-cloned/
 makepkg -sfci --noconfirm --needed
 
-read -r -p "Do you want to install DWM or BSPWM [Select (1) for DWM and (2) for BSPWM]? [1 / 2] " response
+echo -e "[${Yel}*${Whi}] Do you want to install DWM or BSPWM [Select (1) for DWM and (2) for BSPWM]?"
+read -r -p "(1 / 2) -> " response
 if [[ "$response" =~ ^([1])$ ]]
 then
 	mkdir dotfiles-build
@@ -19,12 +30,13 @@ then
 	sudo pacman -S --noconfirm --needed yajl
 	sudo make clean install
 
-	echo "[#################[ Installed DWM ]###################]"
+	echo -e "${Whi}[${Gre}*${Whi}] Installed DWM"
 else
 	sudo pacman -S bspwm
-	echo "[#################[ Installed BSPWM ]###################]"
+	echo -e "${Whi}[${Gre}*${Whi}] Installed BSPWM"
 fi
 
+echo -e "[${Red}+${Whi}] Installing ST"
 # Install ST terminal emulator
 git clone https://github.com/idlidev/xelph-st-git
 cd xelph-st-git/opt/xelph-st-git
@@ -32,6 +44,8 @@ sudo make clean install
 cd ~/.dotfiles
 
 # Install pkgs
+echo -e "[${Red}+${Whi}] Installing all other packages"
+
 sudo pacman -S --noconfirm --needed neovim
 sudo pacman -S --noconfirm --needed sxhkd
 sudo pacman -S --noconfirm --needed ranger
@@ -51,6 +65,8 @@ sudo pacman -S --noconfirm --needed dunst
 sudo pacman -S --noconfirm --needed nodejs
 
 # Install pkgs from the AUR
+echo -e "[${Cya}+${Whi}] Installing AUR packages"
+
 sudo aura -Acax --noconfirm libxft-bgra
 sudo aura -Acax --noconfirm betterlockscreen
 sudo aura -Acax --noconfirm nerd-fonts-jetbrains-mono
@@ -58,18 +74,25 @@ sudo aura -Acax --noconfirm brave-bin
 sudo aura -Acax --noconfirm montserrat-ttf
 sudo aura -Acax --noconfirm polybar-dwm-module
 
+echo -e "[${Gre}+${Whi}] Installing vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -e "[${Gre}*${Whi}] Setting up ZSH"
 
 mkdir -p ~/.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
+# Change shell for root
+sudo chsh -s /usr/bin/zsh
+
 read -r -p "Are you installing this on a virtual machine? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-	echo "[#################[ Finished Installing Packages! ]###################]"
+	echo -e "[${Gre}*${Whi}] Installation has finished!"
 else
+	echo -e "[${Red}+${Whi}] Installing picom"
 	sudo aura -Acax --noconfirm picom-ibhagwan-git
-	echo "[#################[ Finished Installing Packages! ]###################]"
+	echo -e "[${Gre}*${Whi}] Installation has finished!"
 fi
 
