a7848e2f69e67415c72da608141d45cd2de7b98d
Idlidev <idliyout@gmail.com>
2022-01-29 12:58:21 +0530

    [update] update bspwm config tree

--- .config/bspwm/polybar/launch.sh
+++ .config/bspwm/polybar/launch.sh
@@ -3,7 +3,6 @@
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-# polybar -c ~/.config/polybar/config -rq bar &
-polybar -c ~/.config/polybar/config -rq bar &
+polybar -c ~/.config/bspwm/polybar/config -rq bar &
 
 echo "Bars launched..."
