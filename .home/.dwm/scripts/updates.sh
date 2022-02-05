a4d5689687a7c5685febdd4fa68d55d71714dc0a
Idlidev <idliyout@gmail.com>
2022-02-05 08:42:05 +0530

    [update] update updates script

--- .home/.dwm/scripts/updates.sh
+++ .home/.dwm/scripts/updates.sh
@@ -4,20 +4,18 @@ NOTIFY_ICON=/usr/share/icons/numigsur-icon-theme/32/apps/system-software-update.
 
 get_total_updates() { UPDATES=$(checkupdates 2>/dev/null | wc -l); }
 
-while true; do
-    get_total_updates
+get_total_updates
 
-    # notify user of updates
-    if hash notify-send &>/dev/null; then
-        if (( UPDATES > 50 )); then
-            notify-send -a "Updates" -i $NOTIFY_ICON \
-                "You really need to update!!" "$UPDATES New packages"
-        elif (( UPDATES > 25 )); then
-            notify-send -a "Updates" -i $NOTIFY_ICON \
-                "You should update soon" "$UPDATES New packages"
-        elif (( UPDATES > 2 )); then
-            notify-send -a "Updates" -i $NOTIFY_ICON \
-                "$UPDATES New packages"
-        fi
+# notify user of updates
+if hash notify-send &>/dev/null; then
+    if (( UPDATES > 50 )); then
+        notify-send -a "Updates" -i $NOTIFY_ICON \
+            "You really need to update!!" "$UPDATES New packages"
+    elif (( UPDATES > 25 )); then
+        notify-send -a "Updates" -i $NOTIFY_ICON \
+            "You should update soon" "$UPDATES New packages"
+    elif (( UPDATES > 2 )); then
+        notify-send -a "Updates" -i $NOTIFY_ICON \
+            "$UPDATES New packages"
     fi
-done
+fi
