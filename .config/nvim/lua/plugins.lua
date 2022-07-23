5da101ce5c79aaaef5eb35a95862fbed0a44bae3
rv178 <idliyout@gmail.com>
2022-07-23 16:41:09 +0530

    [neovim] don't use vim.cmd in plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,24 +1,26 @@
-vim.cmd([[
-call plug#begin('~/.vim/plugged')
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.vim/plugged')
+
 Plug 'kyazdani42/nvim-web-devicons'
 Plug 'psliwka/vim-smoothie'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'honza/vim-snippets'
 Plug 'Xuyuanp/nerdtree-git-plugin'
-Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'neoclide/coc.nvim'
 Plug 'scrooloose/nerdtree'
 Plug 'airblade/vim-gitgutter'
 Plug 'scrooloose/nerdcommenter'
-Plug 'prettier/vim-prettier', { 'do': 'yarn install' }
+Plug 'prettier/vim-prettier'
 Plug 'arcticicestudio/nord-vim'
 Plug 'vim-airline/vim-airline'
 Plug 'tpope/vim-fugitive'
 Plug 'vim-airline/vim-airline-themes'
 Plug 'nvim-lua/popup.nvim'
-Plug 'nvim-lua/plenary.nvim' " needed for telescope
+Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'ryanoasis/vim-devicons'
 Plug 'github/copilot.vim'
 Plug 'ziglang/zig.vim'
-call plug#end()
-]])
+
+vim.call('plug#end')
