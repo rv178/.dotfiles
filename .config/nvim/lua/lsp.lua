885b797e184bc4ca5904ac01e285abcadae2306a
rv178 <idliyout@gmail.com>
2022-08-30 18:35:59 +0530

    [neovim] use treesitter

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -22,7 +22,12 @@ cmp.setup({
 		-- { name = 'snippy' }, -- For snippy users.
 	}, {
 		{ name = 'buffer' },
-	})
+	}),
+	snippet = {
+		expand = function(args)
+			require'luasnip'.lsp_expand(args.body)
+		end
+	},
 })
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
