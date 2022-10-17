f7150cb13e954a5996dc37ed4b7e0930ccb37a65
rv178 <idliyout@gmail.com>
2022-10-17 20:56:15 +0530

    [update] periodic push

--- .config/nvim/lua/rv178/options.lua
+++ .config/nvim/lua/rv178/options.lua
@@ -25,15 +25,15 @@ vim.opt.title = true
 vim.wo.wrap = false
 
 vim.cmd([[
-autocmd BufWritePre *.js lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.jsx lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.tsx lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.ts lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.rs lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.html lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.css lua vim.lsp.buf.formatting_sync(nil, 100)
-autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 100)
+autocmd BufWritePre *.js lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.jsx lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.tsx lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.ts lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.rs lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.py lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.html lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.css lua vim.lsp.buf.format(nil, 100)
+autocmd BufWritePre *.go lua vim.lsp.buf.format(nil, 100)
 ]])
 
 -- theme settings
