b454ba6c10a6b103ec240f072c65261f6a05e28e
rv178 <idliyout@gmail.com>
2023-01-08 22:00:27 +0530

    [update] periodic push
    * first commit in 2023 :O

--- .config/nvim/lua/plugins/settings/treesitter.lua
+++ .config/nvim/lua/plugins/settings/treesitter.lua
@@ -15,11 +15,11 @@ require('nvim-treesitter.configs').setup {
 	auto_install = true,
 
 	-- List of parsers to ignore installing (for "all")
-	ignore_install = { "javascript" },
+	--ignore_install = { "javascript" },
 
 	highlight = {
 		-- `false` will disable the whole extension
-		enable = true,
+		--enable = true,
 
 		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
 		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
