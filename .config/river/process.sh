2ab79ac79a2025c7ad786cf6ac7772de673b1732
Rohith Vinod <idliyout@gmail.com>
2026-06-21 13:15:48 +0530

    chore(foot): switch to foot server

--- .config/river/process.sh
+++ .config/river/process.sh
@@ -17,6 +17,9 @@ killall wlsunset
 wlsunset -T 4500 &
 brightnessctl set 20%
 
+killall foot
+foot --server &
+
 # River will send the process group of the init executable SIGTERM on exit.
 riverctl default-layout rivertile &
 exec rivertile -main-ratio 0.5 -view-padding 2 -outer-padding 2 &
